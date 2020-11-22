eab0e0d795e2c53f852d44372b1c076c8db6b8fe
mihajlo-perendija <54019549+mihajlo-perendija@users.noreply.github.com>
2020-11-22 14:41:56 +0100

    Master: Initial backend project setup.

---
+++ Backend/Data/Context/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        
+    }
+}
