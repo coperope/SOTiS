f851e4e7a9a2f3995fe126bc39691879582708eb
mihajlo-perendija <54019549+mihajlo-perendija@users.noreply.github.com>
2020-12-13 18:54:42 +0100

    Kst iita with simulated test results

--- kst_service/service/main.py
+++ kst_service/service/main.py
@@ -4,10 +4,9 @@ import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 import sys
-from typing import List
 sys.path.append('learning_spaces/')
 from learning_spaces.kst import iita
-
+import random
 
 async def calculate(dict):
     data_frame = pd.DataFrame(dict)
@@ -19,8 +18,23 @@ app = FastAPI()
 
 
 @app.post("/kst/iita", response_model=str)
-async def create_real_knowledge_space(matrix: dict):
-    result = await calculate(matrix)
+async def create_real_knowledge_space(problems: list):
+
+    dictionary = {}
+    with open('pisa.txt', 'r') as f:
+        questions = f.readline().split()
+
+        for problem in problems:
+            dictionary[problem] = []
+
+        content = f.readlines()
+        content = [x.strip() for x in content]
+        for line in content:
+            split = list(filter(None, line.split(' ')))
+            for (i, problem) in enumerate(problems):
+                dictionary[problem].append(int(split[i+1]) if len(split) > i+1 else random.randint(0, 1))
+
+    result = await calculate(dictionary)
     return json.dumps(result["implications"])
 
 
